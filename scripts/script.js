85eae3d023cbcb293f3cabe43694d13f9cfe4905
Owen <owenkiteck@gmail.com>
2025-02-13 07:34:23 -0600

    CSS now works with ejs files. CSS styles located in public/stylesheets folder

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+app.set('view-engine', 'ejs')
+
+// This alows the ejs files to recognize CSS styling in the public folder
+var path = require('path')
+app.use(express.static('../public'))
+app.use(express.static(path.join(__dirname, "../public")))
+
 
 app.listen(3000)
-
-app.set('view-engine', 'ejs')
-app.use(express.static('../public'))
 
 app.get('/', (req,res) => {
     res.render('index.ejs')
