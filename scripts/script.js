e27b33d174f035c2dbcb58a297deb11f1feee8ff
Owen <owenkiteck@gmail.com>
2025-02-14 22:01:02 -0600

    Made the NavBar UI. Added icons, search bar, user icon/ name, colors. (CSS and EJS for each file updated). No js scripts edited

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const app = express()
 app.set('view-engine', 'ejs')
+// var reload = require('reload')
 
 // This alows the ejs files to recognize CSS styling in the public folder
 var path = require('path')
 app.use(express.static('../public'))
 app.use(express.static(path.join(__dirname, "../public")))
 
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+})
 
-app.listen(3000)
+// reload(app);
 
 app.get('/', (req,res) => {
     res.render('index.ejs')
