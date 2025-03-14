23e20bc3533e80bdec300e9effe70fd4e08ecf8d
Owen <owenkiteck@gmail.com>
2025-03-14 10:51:49 -0500

    Survey is now being displayed on the survey page, though still requires more functionality.

--- scripts/survey.js
+++ scripts/survey.js
@@ -10,16 +10,17 @@ const db = require('./db');
 
 router.get('/survey', (req, res) => {
     const sql = `
-        SELECT surveys.*, survey_questions.*, questions_choices.*
+        SELECT surveys.*, survey_questions.*, 
+        COALESCE(question_choices.question_id, survey_questions.id) AS question_id
         FROM surveys
         JOIN survey_questions ON surveys.id = survey_questions.survey_id
-        JOIN questions_choices ON survey_questions.id = questions_choices.question_id;`
+        LEFT JOIN question_choices ON survey_questions.id = question_choices.question_id;`
     db.query(sql, (err, results) => {
         if (err) {
         console.error("Failed to retrieve data:", err);
         return res.status(500).json({ error: "Failed to retrieve data" });
         }
-        res.render('survey', { surveys: results });
+        res.render('survey', { surveys : results });
     });
 });
 
