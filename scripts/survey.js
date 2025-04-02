049498cac796aca8a53f7af6b2a01b11baaa57e9
Owen <owenkiteck@gmail.com>
2025-04-01 23:10:50 -0500

    Completed the ability to input all types of new questions into a survey. Next i'll make it to where you can delete and re-order questions.

--- scripts/survey.js
+++ scripts/survey.js
@@ -8,25 +8,14 @@ const { log } = require('console');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-// JSDOM.fromURL("your_url").then(dom => {
-//     const document = dom.window.document;
-//     document.addEventListener("DOMContentLoaded", () => {
-//         // Your code to access and manipulate DOM elements goes here
-//         const element = document.querySelector("#myElement");
-//         if (element) {
-//         console.log(element.textContent);
-//         }
-//     });
-//     }).catch(error => {
-//     console.error("Error fetching or parsing the page:", error);
-// });
-
+let SURVEYID;
 
 router.get('/survey', (req, res) => {
     if (req.session.user == null) {
         res.redirect('/login');
         return;
     }
+
 
     const sql = `
         SELECT surveys.*, survey_questions.*, question_choices.*,
@@ -47,6 +36,7 @@ router.post('/surveyForm', (req, res) => {
     // console.log("begin submit");
     const z = req.body;
     // console.log(z);
+    SURVEYID = z.surveyId;
 
     // console.log("inputing into database responses");
     const sql = "INSERT INTO survey_responses (survey_id, responder, res_phone, res_year, res_major, surveyor_id) VALUES (?, ?, ?, ?, ?, ?)";
@@ -79,10 +69,12 @@ router.post('/surveyForm', (req, res) => {
         // console.log(responseId);  // Now this will log after the query finishes
         for (const key in z) {
             if (z.hasOwnProperty(key)) {
-                if (key == "surveyId") { console.log("suceessfull survey form fill"); res.redirect('/survey'); return;} else {
+                if (key == "surveyId") { console.log("suceessfull survey form fill"); res.redirect('/survey'); 
+                    return;
+                } else {
                     const value = z[key];
                     // Do something with key and value
-                    // console.log(`Key: ${key}, Value: ${value}`);
+                    console.log(`Key: ${key}, Value: ${value}`);
     
                     const sql = "INSERT INTO response_answers (response_id, question_id, answer_text) VALUES (?, ?, ?)";
                     db.query(sql, [responseId, parseInt(key), value], (err) => {
@@ -99,6 +91,61 @@ router.post('/surveyForm', (req, res) => {
     
     
 });
+
+router.post('/addQuestion', (req, res) => {
+    const a = req.body;
+    console.log(a);
+    let boolean = false;
+
+    if (a.option1 == null) {boolean = true}
+
+    const sql = "INSERT INTO survey_questions (survey_id, question_text, question_type) VALUES (?, ?, ?)";
+    db.query(sql, [a.surveyId ,a.text, a.type], (err) => {
+        if (err) {
+            console.error("Failed to add survey question:", err);
+            return res.status(500).json({ error: "Failed to add survey question" });
+        }
+    });
+
+    function getResponseId(callback) {
+        const sql2 = `
+            SELECT id FROM survey_questions 
+            ORDER BY id DESC
+            LIMIT 1;`;
+    
+        db.query(sql2, (err, results) => {
+            if (err) {
+                console.error("Failed to get responseId", err);
+                return callback(err, null);
+            }
+            callback(null, results[0].id);  // Pass the result to the callback
+        });
+    }
+    
+    getResponseId((err, responseId) => {
+        if (err) {
+            return res.status(500).json({ error: "Failed to get responseId" });
+        }
+        // console.log(responseId);  // Now this will log after the query finishes
+        for (const key in a) {
+            if (a.hasOwnProperty(key)) {
+                if (key.startsWith("option")) {  
+                    const value = a[key];
+                    console.log(`Key: ${key}, Value: ${value}`);
+
+                    const sql = "INSERT INTO question_choices (choice, question_id) VALUES (?, ?)";
+                    db.query(sql, [value ,responseId], (err) => {
+                        if (err) {
+                            console.error("Failed to add survey question:", err);
+                            return res.status(500).json({ error: "Failed to add survey question" });
+                        }
+                    });
+                }
+            }
+        }
+        res.redirect('/surveyCreate')
+    });
+})
 
 router.get('/surveyCreate', (req, res) => {
     const sql = `
